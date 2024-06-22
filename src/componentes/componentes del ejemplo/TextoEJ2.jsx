7f7689d4677ed8ac718e2926b31dcc5d270ba929
Pietro <beelbonacossa@gmail.com>
2024-06-22 19:08:57 -0300

    arreglando cosas visuales

--- src/componentes/componentes del ejemplo/TextoEJ2.jsx
+++ src/componentes/componentes del ejemplo/TextoEJ2.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import '../../estilos/estilos del ejemplo/TextoEJ2.css'
+import '../../estilos/estilos del ejemplo/TextoEJ2.css';
 
 function TextoEJ2() {
   return (
-    <div className="card-title">
-    <h2>Nuestro trabajo</h2>
-    <h3>Tratamos de buscar una manera de revolucionar la industria tecnológica con innovaciones de vanguardia.</h3>
-  </div>
+    <div className='card-title2'>
+      <h2 className='titulo'>Nuestro trabajo</h2>
+      <h3 className='texto'>Tratamos de buscar una manera de revolucionar la industria tecnológica con innovaciones de vanguardia.</h3>
+    </div>
   );
 }
 
